473f871da104c74fde619837666fc044b5d9c7fd
bailitusu <231820801@qq.com>
2017-04-11 13:54:00 +0800

    taskV1
    this task has some functions:
    1.convertSprcialStringInList
    2.countGoodsNum
    3.receiptList
    4.totalPriceAndFreePrice
    5.printReceipt

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -4,9 +4,9 @@ function printReceipt(inputs) {
   let receipt = statisticalPurchaseList(inputs);
   let allPrice = 0;
   let receiptText = "***<没钱赚商店>收据***\n";
-  for(let i = 0; i < receipt.length; i++) {
+  for(let i in receipt) {
     receiptText = receiptText+"名称："+receipt[i].name+"，数量："+receipt[i].count+receipt[i].unit+"，单价："
-      +receipt[i].price+"(元)，小计："+parseFloat(receipt[i].subtotal).toFixed(2)+"(元)\n"
+      +receipt[i].price+"(元)，小计："+parseFloat(receipt[i].subtotal).toFixed(2)+"(元)\n";
     allPrice = allPrice + receipt[i].subtotal;
   }
   receiptText = receiptText + "----------------------\n"+"总计："+parseFloat(allPrice).toFixed(2)+"(元)\n**********************";
@@ -16,18 +16,17 @@ function printReceipt(inputs) {
 
 function statisticalPurchaseList(inputs) {
   let result = [];
-  for(let i = 0; i < inputs.length; i++) {
-    let tempArray = inputs.filter(function (item) {
-      if(inputs[i].barcode === item.barcode) {
-        return true;
-      }
-    })
+  for(let i in inputs) {
+    let subArray = inputs.filter(function (item) {
+        return inputs[i].barcode === item.barcode;
+    });
     let obj = {};
     obj.name = inputs[i].name;
-    obj.count = tempArray.length;
+    obj.count = subArray.length;
     obj.price = parseFloat(inputs[i].price).toFixed(2) ;
-    obj.subtotal = tempArray.length * inputs[i].price;
+    obj.subtotal = subArray.length * inputs[i].price;
     obj.unit = inputs[i].unit;
+
     if(contains(result,obj) === false) {
       result.push(obj);
     }
--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,110 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+
+function converSpecialStringInList(inputs) {
+  let resultGoodsList = [];
+  for(let item of inputs) {
+    let obj = splitCharacter(item);
+    resultGoodsList.push(obj);
+  }
+  return resultGoodsList;
+}
+
+function splitCharacter(str) {
+  let obj = {};
+  if(str.indexOf("-") >= 0) {
+    obj.barcode = str.split("-")[0];
+    obj.num = isFloat(str.split("-")[1]);
+  }else {
+    obj.barcode = str;
+    obj.num = 1;
+  }
+  return obj;
+}
+
+function isFloat(numStr) {
+  if(numStr.indexOf(".") >= 0) {
+    return parseFloat(numStr);
+  }else {
+    return parseInt(numStr);
+  }
+}
+
+function countGoodsList(resultGoodsList) {
+  let goodsNumList = [];
+  for(let item of resultGoodsList) {
+    let sameItemNum = 0;
+    resultGoodsList.filter( goods => {
+      if(item.barcode === goods.barcode) {
+        sameItemNum = sameItemNum + goods.num;
+      }
+    });
+    let obj = Object.assign({},item,{promotions: judgeAttendDiscount(item)});
+    obj.num = sameItemNum;
+    if(thisArrayContainsThisObj(goodsNumList,obj) === false) {
+      goodsNumList.push(obj);
+    }
+  }
+  return goodsNumList;
+}
+
+function judgeAttendDiscount(item) {
+  let promotionsArray = loadPromotions();
+  for(let promotion of promotionsArray) {
+    if(promotion.type === "BUY_TWO_GET_ONE_FREE") {
+      return promotion.barcodes.includes(item.barcode);
+    }
+  }
+  return false;
+}
+
+function receiptList(goodsNumList) {
+
+  return goodsNumList.map(goods => {
+    for(let item of loadAllItems()) {
+      if(item.barcode === goods.barcode) {
+        goods = Object.assign({},item,{num:goods.num},{promotions:goods.promotions});
+      }
+    }
+    if(goods.promotions === true){
+      goods.subtotal = (goods.num - (goods.num - goods.num % 3) / 3) * goods.price;
+    }else {
+      goods.subtotal = goods.num * goods.price;
+    }
+    goods.freePrice = goods.num * goods.price - goods.subtotal;
+    return goods;
+  });
+
+}
+
+function totalPriceAndFreePrice(result) {
+  let totalPrice = 0;
+  let freePrice = 0;
+  for (let item of result) {
+    totalPrice = item.subtotal + totalPrice;
+    freePrice = item.freePrice + freePrice;
+  }
+  return {totalPrice: totalPrice, freePrice: freePrice};
+}
+
+function printReceipt(inputs) {
+  let receiptArray = receiptList(countGoodsList(converSpecialStringInList(inputs)));
+  let obj = totalPriceAndFreePrice(receiptArray);
+  let receiptText = "***<没钱赚商店>收据***\n";
+  for(let item of receiptArray) {
+    receiptText = receiptText+`名称：${item.name}，数量：${item.num}${item.unit}，单价：${parseFloat(item.price).toFixed(2)}(元)，`
+      +`小计：${parseFloat(item.subtotal).toFixed(2)}(元)`+"\n";
+  }
+  receiptText = receiptText + `----------------------\n总计：${parseFloat(obj.totalPrice).toFixed(2)}(元)\n`
+    +`节省：${parseFloat(obj.freePrice).toFixed(2)}(元)\n**********************`;
+  console.log(receiptText);
+}
+
+function thisArrayContainsThisObj(inputArray,obj) {
+  for(let item of inputArray) {
+    if(item.barcode === obj.barcode) {
+      return true;
+    }
+  }
+  return false;
+}
